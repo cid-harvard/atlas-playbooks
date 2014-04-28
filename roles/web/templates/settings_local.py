a7c82bbaaa43b5d21ffcf0901569374d86540552
Mali Akmanalp <mali@akmanalp.com>
2014-04-28 16:33:17 -0400

    Add more stuff to settings_local

--- roles/web/templates/settings_local.py
+++ roles/web/templates/settings_local.py
@@ -32,7 +32,10 @@ CACHES = {
 }
 
 HTTP_HOST = '/'
-DB_PREFIX = ''
+DB_PREFIX = 'new_'
+STATIC_IMAGE_MODE = 'PNG'
+DATA_FILES_PATH = '/tmp/'
+INTERNAL_IPS = ('127.0.0.1','192.168.33.1', '10.0.2.2') #For Django debug toolbar
 
 # FOR VERBOSE JS OUTPUT
 DEV = False
